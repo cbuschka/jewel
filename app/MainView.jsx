d3a5df5eb5d8ff3b6c4d17a6f5cf7f57b2aab706
conni <c.buschka@arcusx.com>
2018-04-27 17:38:40 +0200

    Webpack/react setup.

---
+++ app/MainView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class MainView extends React.Component {
+    render() {
+        return (<div><h1>Hello</h1></div>);
+    }
+}
---
+++ app/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import MainView from './MainView';
+
+render(<MainView/>, document.getElementById('root'));
